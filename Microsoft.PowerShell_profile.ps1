67475823b6b016bf913af2b866078fbff5e196d1
GEROMAX <tobita0112@gmail.com>
2018-05-16 16:41:23 +0900

    update powershell profile.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
+#powershellのプロファイルのパスはps上で$profileで確認できる
+
+#curlのエイリアス設定(DL & 配置済みが条件)
 Set-Alias curl "C:\curl\bin\CURL.EXE"
 
+#touchコマンドの再現
 function touch()
 {
   New-Item $args[0] -itemType File
